05eb21dc93558f80fbab6c7812a815ada0366077
Sahil Ishar <sahil.ishar@gmail.com>
2015-12-28 18:12:06 -0500

    Updated to handle user info in app.js, plus added router.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var router = require("./router.js");
+//Problem: We need a simple way to look at a user's badge count and JavaScript points from a web browser
+//Solution: Use Node.js to perform the profile look ups and server our template via HTTP
+
+//Create a web server
+var http = require('http');
+http.createServer(function(request, response) {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(3000);
+console.log('Server running at http:<workspace-url>/');
+
+//Function that handles the reading of files and merge in values
+	//read from file and get a string
+		//merge values in to string
---
+++ router.js
@@ -0,0 +1,56 @@
+var Profile = require("./profile.js");
+
+
+//Handle HTTP route GET / and POST / i.e. Home
+function home(request, response) {
+	//if url == "/" && GET
+	if (request.url === "/") {
+		//show search
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		response.write("Header\n");
+		response.write("Search\n");
+  		response.end('Footer\n');
+	}
+	//if url == "/" && POST
+		//redirect to /:username
+}
+	
+//Handle HTTP route GET /:username i.e. /sahilishar
+function user(request, response) {
+	//if url == "/...."
+	var username = request.url.replace("/", "");
+	if (username.length > 0) {
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		response.write("Header\n");
+
+		//get json from Treehouse
+		var studentProfile = new Profile(username);
+		//on "end"
+		studentProfile.on("end", function(profileJSON) {
+			//show profile
+
+			//store the values which we need
+			var values = {
+				avatarUrl: profileJSON.gravatar_url,
+				username: profileJSON.profile_name,
+				badges: profileJSON.badges.length,
+				javascriptPoints: profileJSON.points.JavaScript
+			}
+
+			//simple response
+			response.write(values.username + " has " + values.badges + " badges\n");
+  			response.end('Footer\n');
+		});
+			
+		//on "error"
+		studentProfile.on("error", function(e) {
+			//show error
+			response.write(e.message + "\n");
+  			response.end('Footer\n');
+		});
+	}
+}
+
+
+module.exports.home = home;
+module.exports.user = user;
